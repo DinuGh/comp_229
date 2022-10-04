a77521e61a6d0e5df8009b917fc27fe7389c74de
Dinu <dinugherciu93@gmail.com>
2022-10-04 15:09:44 -0400

    final changes

--- routes/index.js
+++ routes/index.js
@@ -3,33 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Home' });
 });
 
-router.get('/dinu', function(req, res, next) {
-  res.render('index', { title: 'Dinu' });
+/* GET Products page. THIS IS ONE ROUTE AND IT IS TO GET TO PROJECTS / ADDED NEW VARIABLES FOR DESCRIPTIONS OF PROJECTS*/
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects'});
 });
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render(
-//     'index', 
-//     { 
-//       title: 'Home', 
-//       name: 'Julio'
-//     }
-//   );
-// });
+/* GET Services page. THIS IS ONE ROUTE AND IT IS TO GET TO SERVICES*/
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services'});
+});
 
-// router.get('/projects', function(req, res, next) {
-//   res.render('main',  { 
-//     title: 'Projects', 
-//     name: 'Julio'
-//   });
-// });
+/* GET About Me page. THIS IS ONE ROUTE AND IT IS TO GET TO ABOUT ME PAGE*/
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me',
+name:"Dinu"});
+});
+
+/* GET Contact Us page. THIS IS ONE ROUTE AND IT IS TO GET TO CONTACT ADDED VARIABLES FOR MESSAGES IN CONTACT PAGE*/
+router.get('/contacts', function(req, res, next) {
+  res.render('contacts', { title: 'Contacts',
+message:'Leave me a message' });
+});
 
 module.exports = router;
 
-//Run "nodemon" and access this URL: http://localhost:3000/dinu
+//Run "nodemon" and access this URL: http://localhost:3000/projects
 
 //npmjs.com to take fonts
