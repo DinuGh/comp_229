638b35bfde1e3e24d5c4211abaaeeb7a4ca1f123
Dinu <dinugherciu93@gmail.com>
2022-10-05 14:55:18 -0400

    refactor and updated partials

--- routes/index.js
+++ routes/index.js
@@ -3,29 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Home' });
+  res.render('home', { 
+  title: 'Home',
+  name:"Dinu"});
 });
 
 /* GET Products page. THIS IS ONE ROUTE AND IT IS TO GET TO PROJECTS / ADDED NEW VARIABLES FOR DESCRIPTIONS OF PROJECTS*/
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { 
+title: 'Projects',
+uiDemo: 'UI Demo',
+loadDemo: 'Load Demo'});
 });
 
 /* GET Services page. THIS IS ONE ROUTE AND IT IS TO GET TO SERVICES*/
 router.get('/services', function(req, res, next) {
-  res.render('services', { title: 'Services'});
+  res.render('services', {
+title: 'Services',
+performance: 'Performance',
+stress: 'Stress',
+uiUx: 'UI/UX',
+api: 'API',
+bdd: 'BDD',
+unit: 'Unit',
+mobile: 'Mobile',
+penetration: 'Penetration'});
 });
 
 /* GET About Me page. THIS IS ONE ROUTE AND IT IS TO GET TO ABOUT ME PAGE*/
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'About Me',
-name:"Dinu"});
+  res.render('about', { title: 'About Me'});
 });
 
 /* GET Contact Us page. THIS IS ONE ROUTE AND IT IS TO GET TO CONTACT ADDED VARIABLES FOR MESSAGES IN CONTACT PAGE*/
 router.get('/contacts', function(req, res, next) {
-  res.render('contacts', { title: 'Contacts',
-message:'Leave me a message' });
+  res.render('contacts', { 
+title: 'Contacts',
+message:'Fan? Drop a note!'});
 });
 
 module.exports = router;
